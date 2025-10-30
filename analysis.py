637853e6586d7c3e9e6b57c8b45164982efd6780
Amy Kim <amy.kim@stonybrook.edu>
2025-10-29 21:44:35 -0400

    start

---
+++ analysis.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+# Question 1: Is there an association between marital status (married or not married) 
+# and education level (bachelor’s degree or higher vs. less than a bachelor’s degree)?
+
